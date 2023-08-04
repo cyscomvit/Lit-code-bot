92139871b3652048fcc8e9192e1c0873503e474d
Karthik Chandrasekhar <karthikchandrasekhar03@outlook.com>
2023-08-04 10:52:15 +0530

    Fetch user details from Google Sheets

--- index.js
+++ index.js
@@ -10,53 +10,37 @@ const client = new Client({
         GatewayIntentBits.MessageContent
 	],
 });
-
-
+let memberScores = {}
+const SHEET_LINK = '1uP9kyrUGwV80C0Dy8NCfPlJoUcQ7zTCaAaBJgB3IqbU'
+const SHEET_API = `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_LINK}/values/Sheet1?alt=json&key=${process.env.SHEET_API_KEY}`
 let channelId = ''
 client.on("ready", () => {
     console.log('Logged in as ' + client.user.tag)
     channelId = client.channels.cache.get("1136692829410820128")
-    updateScores()
+    getSolveStatus()
     
 })
 
 
-let memberDetails = [
-    {
-        name:'Karthik Chandrasekhar',
-        siteId:'Karthik2025',
-        count:0   
-    }
-]
 client.on("messageCreate", msg => {
     console.log('message received in channel')
+    
 })
 
-async function getSolveStatus(userid) {
-    console.log(userid['name'])
-    let profileUrl = 'https://faisal-leetcode-api.cyclic.app/' + userid['siteId']
-    let data = await fetch(profileUrl)
-    let jsonData = await data.json()
-    let score = jsonData['totalSolved']
-    console.log(score + ' ' + userid['count'] + typeof(score))
-    if (score > userid['count']) {
-        //console.log(channelId)
-        let diff = score-userid['count']
-        let display = userid['name'] + " solved " + diff + " more problems!"
-        channelId.send(display)
-        userid['count'] = score
-        return 1
-    }
-    return 0
-}
+async function getSolveStatus() {
+    let memberdata = await fetch(SHEET_API)
+    let memberJsonData = await memberdata.json()
+    //console.log(memberJsonData)
+    for (let i = 1; i < memberJsonData['values'].length; i++) {
+        // console.log(memberJsonData['values'][i][0] + ' ' + memberJsonData['values'][i][1])
+        let profileUrl = 'https://faisal-leetcode-api.cyclic.app/' + memberJsonData['values'][i][1]
 
-async function updateScores() {
-    for (let i = 0; i < memberDetails.length; i++) {
-        await getSolveStatus(memberDetails[i])
+        // Find the number of problems solved by the member
+        let data = await fetch(profileUrl)
+        let jsonData = await data.json()
+        let score = jsonData['totalSolved']
+        console.log(memberJsonData['values'][i][0] + ' solved ' + score + ' problems till now.')
     }
-    console.log(memberDetails)
-    
 }
-
 
 client.login(process.env.TOKEN)
