eb9ae88440d6f66532c3d22d7d42527a445976c0
Karthik Chandrasekhar <karthikchandrasekhar03@outlook.com>
2023-09-06 11:38:04 +0530

    Restructure

--- tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-let scoreData = fs.readFileSync("scores.json")
-if (typeof(JSON.parse(scoreData)) ==='object') {
-    scoreObject = []
-    scoreObject.push(JSON.parse(scoreData))
-} else {
-    scoreObject = JSON.parse(scoreData)
-}
